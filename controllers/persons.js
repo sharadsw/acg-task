b8bb5be7d442ddccef711e2f44704d3b43fa4ae4
TheRealSS <sharad97@gmail.com>
2020-11-26 00:10:33 +0530

    Add router for persons

---
+++ controllers/persons.js
@@ -0,0 +1,54 @@
+const personRouter = require('express').Router()
+const Person = require('../models/person')
+
+personRouter.get('/', async (request, response) => {
+  const people = await Person.find({})
+
+  response.json(people)
+})
+
+personRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  if (!body.fname || !body.lname) {
+    response.status(400).send({ error: 'first or last name missing' })
+  }
+
+  const person = new Person({
+    fname: body.fname,
+    lname: body.lname
+  })
+
+  const result = await person.save()
+  response.status(201).json(result)
+})
+
+personRouter.put('/:id', async (request, resposne) => {
+  const body = request.body
+
+  if (!body.fname || !body.lname) {
+    response.status(400).send({ error: 'first or last name missing' })
+  }
+
+  const person = {
+    fname: body.fname,
+    lname: body.lname
+  }
+
+  const updatedPerson = await Person.findByIdAndUpdate(request.params.id, person, { new:true, runValidators: true })
+  if (updatedPerson) {
+    response.json(updatedPerson)
+  } else {
+    response.status(404).end()
+  }
+})
+
+personRouter.delete('/:id', async (request, response) => {
+  await Person.findByIdAndDelete(request.params.id)
+
+  response.status(204).end()
+})
+
+module.exports = {
+  personRouter
+}
--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
-if (process.env.MONGODB_URI === 'test') {
+if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.MONGODB_URI_TEST
 }
 
