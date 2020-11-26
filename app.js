0fcc724dc1cce1cdf559106a0bbbfcd5819c7ea6
TheRealSS <sharad97@gmail.com>
2020-11-27 00:10:03 +0530

    Add build files for heroku

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     console.log('Error in connection', error.message);
   })
 
-app.use(express.static('ui/build'))
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
